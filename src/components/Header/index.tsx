2898d2e8b59cd34b9028c106ac6f357a74133252
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2023-07-06 09:35:13 -0700

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { HeaderContainer } from './style'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
